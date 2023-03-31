6f845bffbd5410f6fd875aff816103e154055356
JuanpiAMVs <juenpablogodoyn@gmail.com>
2023-03-31 02:17:18 -0300

    Product class

---
+++ Clase 03/desafio1.js
@@ -0,0 +1,42 @@
+class ProductManager {
+    constructor(){
+        this.products= []
+    }
+
+    addProduct(title, description, price, thumbnail, code, stock){
+        const product = {
+            title: title,
+            description: description,
+            price: price,
+            thumbnail: thumbnail,
+            code: code,
+            stock: stock
+        }
+        const findCode = this.products.find((obj) => obj.code === code)
+            if (findCode){
+                console.log(`El codigo ${findCode.code} ya se encuentra ocupado`)
+            }else{
+                product.id = this.products.length > 0 ? this.products[this.products.length-1].id + 1 : 1
+                this.products.push(product)
+                console.log("Producto agregado")
+            }
+        }
+        
+
+        getProducts(){
+            console.log(this.products)
+            return this.products
+        }
+    
+        getProductById(id){
+            const filter = this.products.filter(e => e.id===id)
+            if(filter.length == 0) console.log(`id ${id} Not found `)
+            return filter
+        }
+    
+    }
+
+let productos = new ProductManager()
+productos.addProduct("mondongo","asdkjashdak", 12312, "/f:asdsad/asdas", "21a", 1 )
+productos.addProduct("mondongo","asdkjashdak", 12312, "/f:asdsad/asdas", "2a", 1 )
+productos.getProducts()
