07a6e999fc495d80867d1b14ce6c3c430c053278
JuanpiAMVs <juenpablogodoyn@gmail.com>
2023-04-08 22:38:16 -0300

    Class ProductManager

---
+++ Clase 05/ProductManager.js
@@ -0,0 +1,110 @@
+import fs from "fs";
+
+export default class ProductManager {
+  constructor(path) {
+    this.path = path;
+  }
+
+  async addProduct(obj) {
+    try {
+      if (
+        !obj.title ||
+        !obj.description ||
+        !obj.price ||
+        !obj.thumbnail ||
+        !obj.code ||
+        !obj.stock
+      ) {
+        console.log("Faltan argumentos al agregar producto");
+        return;
+      }
+      const product = obj;
+      const products = await this.getProducts();
+      const findCode = products.find((product) => product.code === obj.code);
+      if (findCode) {
+        console.log(`El codigo ${findCode.code} ya se encuentra ocupado`);
+      } else {
+        product.id =
+          products.length > 0 ? products[products.length - 1].id + 1 : 1;
+        products.push(product);
+        const writeProducts = await fs.promises.writeFile(
+          this.path,
+          JSON.stringify(products, null, "\t")
+        );
+        console.log("Producto agregado");
+        return product;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getProducts() {
+    try {
+      if (fs.existsSync(this.path)) {
+        const data = await fs.promises.readFile(this.path, "utf-8");
+        const produts = JSON.parse(data);
+        return produts;
+      }
+      console.log(`El archivo de la ruta ${this.path} no existe`);
+      return [];
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async getProductById(id) {
+    try {
+      if (!id) {
+        console.log("Falta ingresar ID");
+        return;
+      }
+      const products = await this.getProducts();
+      const findProduct = products.find((products) => products.id === id);
+      if (!findProduct) {
+        console.log(`El producto con el ID ${id} no se ha encontrado`);
+        return;
+      }
+      console.log(findProduct);
+      return findProduct;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async updateProduct(id, atribute, value) {
+    try {
+      if (!id || !atribute || !value) {
+        console.log("Faltan argumentos");
+        return;
+      }
+      const products = await this.getProducts();
+      const indexFind = products.findIndex((product) => product.id === id);
+      const productSelected = products[indexFind];
+      const productUpdate = (productSelected[atribute] = value);
+      const writeProducts = await fs.promises.writeFile(
+        this.path,
+        JSON.stringify(products, null, "\t")
+      );
+      console.log("Producto agregado y actualizado");
+      return productUpdate;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async deleteProduct(id) {
+    try {
+      const products = await this.getProducts();
+      const newProducts = products.filter((product) => product.id !== id);
+      const writeProducts = await fs.promises.writeFile(
+        this.path,
+        JSON.stringify(newProducts, null, "\t")
+      );
+      console.log(`Producto con ID ${id} borrado y lista actualizada`);
+      return;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
---
+++ Clase 05/index.js
@@ -0,0 +1,26 @@
+import ProductManager from "./ProductManager.js";
+
+const productManager = new ProductManager("./products.json");
+
+const context = async () => {
+  try {
+/*     const getProducts = await productManager.getProducts();
+    console.log(getProducts); */
+    let testProduct = {
+      title: "asfasdasdas",
+      description: "asdkjashdak",
+      price: 12312,
+      thumbnail: "/f:asdsad/asdas",
+      code: "21s",
+      stock: 2,
+    };
+/*     await productManager.addProduct(testProduct);
+    await productManager.getProductById(1) */
+/*     await productManager.updateProduct(1, "title", "asdasdsadasd") */
+        await productManager.deleteProduct(2)
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+context()
