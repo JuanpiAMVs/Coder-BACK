5d894e1e2932a0590b7a2989c506d6162d913a70
JuanpiAMVs <89683170+JuanpiAMVs@users.noreply.github.com>
2023-10-03 02:33:26 -0300

    Update error.js

--- Entrega final/src/middlewares/error.js
+++ Entrega final/src/middlewares/error.js
@@ -2,4 +2,5 @@ import EErrors from "../constants/EErrors.js";
 
 export default (err, req, res, next) => { // Este es el que define que NUNCA caiga el server
   res.send({ status: "error", error: err.message });
+  console.log(err)
 };
